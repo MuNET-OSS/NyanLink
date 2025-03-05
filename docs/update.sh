7bd892371955fdcf549014210876977bcf07f213
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-03-05 08:04:07 -0500

    [F] Fix update script

--- docs/update.sh
+++ docs/update.sh
@@ -9,19 +9,21 @@ GITHUB_REPO="worldlinkd"
 INSTALL_DIR="/app/wl"
 JAR_NAME="worldlinkd.jar"
 
-echo "[INFO] Fetching the latest release data from GitHub..."
-LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases/latest)
+# echo "[INFO] Fetching the latest release data from GitHub..."
+# LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases/latest)
+#
+# # Parse the .jar download URL from the release assets
+# DOWNLOAD_URL=$(echo "${LATEST_RELEASE_JSON}" \
+#   | grep "browser_download_url" \
+#   | grep ".jar" \
+#   | cut -d '"' -f 4)
+#
+# if [ -z "${DOWNLOAD_URL}" ]; then
+#   echo "[ERROR] Unable to find a .jar download URL in the latest release."
+#   exit 1
+# fi
 
-# Parse the .jar download URL from the release assets
-DOWNLOAD_URL=$(echo "${LATEST_RELEASE_JSON}" \
-  | grep "browser_download_url" \
-  | grep ".jar" \
-  | cut -d '"' -f 4)
-
-if [ -z "${DOWNLOAD_URL}" ]; then
-  echo "[ERROR] Unable to find a .jar download URL in the latest release."
-  exit 1
-fi
+DOWNLOAD_URL="https://github.com/MewoLab/worldlinkd/releases/latest/download/worldlinkd.jar"
 
 echo "[INFO] Downloading JAR from: ${DOWNLOAD_URL}"
 curl -L -o "${INSTALL_DIR}/${JAR_NAME}" "${DOWNLOAD_URL}"
